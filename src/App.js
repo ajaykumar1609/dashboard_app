321fcf53447ec860fabd26d15261c32350df417b
Ajaykumar1609 <kotaajaykumar@iitbhilai.ac.in>
2023-01-08 20:51:51 +0530

    sidebar update

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { FiSettings } from "react-icons/fi";
 import { TooltipComponent, Position } from "@syncfusion/ej2-react-popups";
 import { Navbar, Footer, Sidebar, ThemeSettings } from "./components";
+import { useStateContext } from "./contexts/ContextProvider";
 import {
   Ecommerce,
   Orders,
@@ -23,7 +24,7 @@ import {
 } from "./pages";
 import "./App.css";
 const App = () => {
-  const activeMenu = true;
+  const {activeMenu} = useStateContext();
   return (
     <div>
       <BrowserRouter>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import React from "react";
 import  ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ContextProvider } from "./contexts/ContextProvider";
+// ReactDOM.render(<ContextProvider>
+//     <App/>
+// </ContextProvider>,document.getElementById('root'));
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<ContextProvider>
+    <App/>
+</ContextProvider>);
